ed0689366f8cdee419f83513c664eed64903c5b4
rydevin <rydevin@gmail.com>
2026-07-02 14:46:29 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys()) await caches.delete(k); await self.clients.claim();})())});
+const CACHE='waypoint-3-0';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon.svg'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
