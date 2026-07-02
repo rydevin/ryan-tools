857cca365e37227a8e10a50f410f7cfb196dd8b2
rydevin <rydevin@gmail.com>
2026-07-02 13:33:37 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='waypoint-v1-2';
+const CACHE='waypoint-v1.3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./Everley_Hunt_GALLERY.html','./Ethan_Hunt_GALLERY.html','./Elizabeth_Hunt_GALLERY.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
