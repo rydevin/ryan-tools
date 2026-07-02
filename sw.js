1110ebeb4c280e67c02f5aa7f4a35dfde868a1ab
rydevin <rydevin@gmail.com>
2026-07-02 14:36:37 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
-// Waypoint 2.2.2: no caching while troubleshooting blank screen.
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys()) await caches.delete(k); await self.clients.claim();})())});
